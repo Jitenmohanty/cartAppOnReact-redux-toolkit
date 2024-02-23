56ae5204b982ee2d70b6a88e0cdfea99e70f9f2c
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2024-02-23 11:43:37 +0530

    Add it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { useSelector } from "react-redux";
+import Logo from '../assets/Logo.avif';
 
 const Header = () => {
   const {cartItems} = useSelector(state=>state.cart)
   return (
     <nav>
+      {/* <img src={Logo}/> */}
       <h2>TrendyFunk..</h2>
       <div>
         <Link to={"/"}>Home</Link>
